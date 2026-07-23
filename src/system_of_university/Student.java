efd3a4ef10cfdb08b6f91aa989a791300f53dc93
Sarah Yusuf <sarah.yusuf.abdullatif@gmail.com>
2026-07-23 22:59:21 +0300

    Student.java

--- src/system_of_university/Student.java
+++ src/system_of_university/Student.java
@@ -1,13 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package system_of_university;
 
 import java.util.ArrayList;
 
-//  @author Shams
+
 public class Student extends Person {
 
     private static long counter = 202600000;
