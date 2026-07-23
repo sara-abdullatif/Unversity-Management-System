698ef89dbf438ff9945fcde7a1039f1eac021c31
Sarah Yusuf <sarah.yusuf.abdullatif@gmail.com>
2026-07-23 22:58:26 +0300

    Lecturer.java

--- src/system_of_university/Lecturer.java
+++ src/system_of_university/Lecturer.java
@@ -1,13 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package system_of_university;
 
 import java.util.ArrayList;
 
-//  @author Retage
+
 public class Lecturer extends Person {
 
     private static long counter = 100;
