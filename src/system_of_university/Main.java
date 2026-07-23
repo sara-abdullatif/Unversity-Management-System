16b8566a1fa8e2206eb8dfc4f43450170a8bc9e0
Sarah Yusuf <sarah.yusuf.abdullatif@gmail.com>
2026-07-23 22:58:43 +0300

    Main.java

--- src/system_of_university/Main.java
+++ src/system_of_university/Main.java
@@ -1,16 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package system_of_university;
 
 import java.util.Scanner;
 
-/**
- *
- * @author sara
- */
+
 public class Main {
 
     public static void main(String[] args) {
