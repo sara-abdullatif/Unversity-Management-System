28544974be1b5389bc5a0005cf6294a1f45a905c
Sarah Yusuf <sarah.yusuf.abdullatif@gmail.com>
2026-07-23 22:57:57 +0300

    Update Course.java

--- src/system_of_university/Course.java
+++ src/system_of_university/Course.java
@@ -1,11 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package system_of_university;
 
-//  @author Reem
 public class Course {
 
     private final String courseCode;
