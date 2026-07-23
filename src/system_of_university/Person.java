1abe32422b7873e39c154548945be501b9b07d24
Sarah Yusuf <sarah.yusuf.abdullatif@gmail.com>
2026-07-23 22:58:58 +0300

    Update Person.java

--- src/system_of_university/Person.java
+++ src/system_of_university/Person.java
@@ -1,11 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package system_of_university;
 
-//  @author Sara
+
 public abstract class Person {
 
     private String name;
